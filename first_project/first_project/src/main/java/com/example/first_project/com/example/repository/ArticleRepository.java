fd18e5569072b2ab9e32f1cb3193fb1aebf30f2f
gjrjsghl <workanchovy@gmail.com>
2026-05-12 04:24:54 +0900

    0506 spring

---
+++ first_project/first_project/src/main/java/com/example/first_project/com/example/repository/ArticleRepository.java
@@ -0,0 +1,8 @@
+package com.example.first_project.com.example.repository;
+
+import com.example.first_project.entity.Article;
+import org.springframework.data.repository.CrudRepository;
+
+public interface  ArticleRepository extends CrudRepository<Article,Long> {
+
+}
--- first_project/first_project/src/main/java/com/example/first_project/dto/ArticleForm.java
+++ first_project/first_project/src/main/java/com/example/first_project/dto/ArticleForm.java
@@ -1,5 +1,7 @@
 package com.example.first_project.dto;
 
+import com.example.first_project.entity.Article;
+
 public class ArticleForm {
     private String title;
     private String content;
@@ -15,5 +17,9 @@ public class ArticleForm {
                 "title='" + title + '\'' +
                 ", content='" + content + '\'' +
                 '}';
+    }
+
+    public Article toEntity() {
+        return new Article(null,title,content);
     }
 }
